3ee45f402946ce8283b38be3b05eca0cb9e7f75f
Ivan Misyats <ivan@misyats.com>
2014-07-15 23:21:27 +0300

    tm_super_profile_PT-PT.user.js: * Fixed script, localised to en-US, improved formatting

--- tm_super_profile_PT-PT.user.js
+++ tm_super_profile_PT-PT.user.js
@@ -6,7 +6,7 @@
 // @include		https://trophymanager.com/players/*
 // @exclude		https://trophymanager.com/players
 // @author    	  Joao Manuel Ferreira Fernandes
-// @github		  http://github.com/etnepres/trophymanager.git
+// @github		  https://github.com/IvanMisyats/trophymanager
 // @grant 		none
 // ==/UserScript==
 
@@ -14,42 +14,43 @@
 // Array to setup the weights of particular skills for each player's actual ability
 // This is the direct weight to be given to each skill.
 // Array maps to these skills:
-//			   [For,Res,Vel,Mar,Tac,TrE,Pos,Pas,Cru,Tec,Cab,Fin,RmL,BP]
-var positions = [[  1,  3,  1,  1,  1,  3,  3,  2,  2,  2,  1,  3,  3,  3], // D C
-		 [  2,  3,  1,  1,  1,  3,  3,  2,  2,  2,  2,  3,  3,  3], // D E
-		 [  2,  3,  1,  1,  1,  3,  3,  2,  2,  2,  2,  3,  3,  3], // D D
-		 [  1,  2,  2,  1,  1,  1,  1,  1,  2,  2,  1,  3,  3,  3], // MD C
-		 [  2,  3,  1,  1,  1,  3,  3,  2,  2,  2,  2,  3,  3,  3], // MD E
-		 [  2,  3,  1,  1,  1,  3,  3,  2,  2,  2,  2,  3,  3,  3], // MD D
+//		 [Str,Sta,Pac,Mar,Tac,Wor,Pos,Pas,Cro,Tec,Hea,Fin,Lon,Set]
+var positions = 
+		[[  1,  3,  1,  1,  1,  3,  3,  2,  2,  2,  1,  3,  3,  3], // D C
+		 [  2,  3,  1,  1,  1,  3,  3,  2,  2,  2,  2,  3,  3,  3], // D L
+		 [  2,  3,  1,  1,  1,  3,  3,  2,  2,  2,  2,  3,  3,  3], // D R
+		 [  1,  2,  2,  1,  1,  1,  1,  1,  2,  2,  1,  3,  3,  3], // DM C
+		 [  2,  3,  1,  1,  1,  3,  3,  2,  2,  2,  2,  3,  3,  3], // DM L
+		 [  2,  3,  1,  1,  1,  3,  3,  2,  2,  2,  2,  3,  3,  3], // DM R
 		 [  2,  2,  3,  1,  1,  1,  1,  1,  3,  1,  2,  3,  3,  3], // M C 
-		 [  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  2,  3,  3,  3], // M E
-		 [  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  2,  3,  3,  3], // M D
-		 [  2,  3,  3,  2,  2,  1,  1,  1,  3,  1,  2,  1,  1,  3], // MO C
-		 [  2,  2,  1,  3,  3,  2,  2,  3,  1,  1,  2,  2,  2,  3], // MO E
-		 [  2,  2,  1,  3,  3,  2,  2,  3,  1,  1,  2,  2,  2,  3], // MO D
-		 [  1,  2,  2,  3,  3,  2,  2,  3,  3,  2,  1,  1,  1,  3], // A
-		 [  2,  3,  2,  1,  2,  1,  2,  2,  3,  3,  3]]; // GR
+		 [  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  2,  3,  3,  3], // M L
+		 [  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  2,  3,  3,  3], // M R
+		 [  2,  3,  3,  2,  2,  1,  1,  1,  3,  1,  2,  1,  1,  3], // OM C
+		 [  2,  2,  1,  3,  3,  2,  2,  3,  1,  1,  2,  2,  2,  3], // OM L
+		 [  2,  2,  1,  3,  3,  2,  2,  3,  1,  1,  2,  2,  2,  3], // OM R
+		 [  1,  2,  2,  3,  3,  2,  2,  3,  3,  2,  1,  1,  1,  3], // F
+		 [  2,  3,  2,  1,  2,  1,  2,  2,  3,  3,  3]]; // Gk
 
 
 // [  2,  3,  2,  1,  2,  1,  2,  2,  3,  3,  3]
 // Weights need to total 100
 var weights = [ [85,12, 3],  // D C
-		[70,25, 5],  // D E
-		[70,25, 5],  // D D
-		[90,10, 0],  // MD C
-		[50,40,10],  // MD E
-		[50,40,10],  // MD D
+		[70,25, 5],  // D L
+		[70,25, 5],  // D R
+		[90,10, 0],  // DM C
+		[50,40,10],  // DM L
+		[50,40,10],  // DM R
 		[85,12, 3],  // M C			   
-		[90, 7, 3],  // M E
-		[90, 7, 3],  // M D
-		[90,10, 0],  // MO C
-		[60,35, 5],  // MO E
-		[60,35, 5],  // MO D
-		[80,18, 2], // A
+		[90, 7, 3],  // M L
+		[90, 7, 3],  // M R
+		[90,10, 0],  // OM C
+		[60,35, 5],  // OM L
+		[60,35, 5],  // OM R
+		[80,18, 2],  // F
 		[50,42, 8]]; // GK
 
-var positionNames = ["D C", "D E", "D D", "MD C", "MD E", "MD D", "M C", "M E", "M D", "MO C", "MO E", "OM D", "A", "GR"];
-var positionFullNames = ["Defesa Centro", "Defesa Esquerdo", "Defesa Direito", "Médio Defensivo Centro", "Médio Defensivo Esquerdo", "Médio Defensivo Direito", "Medio Centro", "Medio Esquerdo", "Medio Direito", "Medio Ofensivo Centro", "Medio Ofensivo Esquerdo", "Medio Ofensivo Direito", "Avançado", "Guarda-Redes"];
+var positionNames = ["D C", "D L", "D D", "DM C", "DM L", "DM R", "M C", "M L", "M R", "OM C", "OM L", "OM R", "F", "Gk"];
+var positionFullNames = ["Defender Center", "Defender Left", "Defender Right", "Defensive Midfielder Center", "Defensive Midfielder Left", "Defensive Midfielder Right", "Midfielder Center", "Midfielder Left", "Midfielder Right", "Offensive Midfielder Center", "Offensive Midfielder Left", "Offensive Midfielder Right", "Forward", "Goalkeeper"];
 
 if (location.href.indexOf("/players/") != -1){
 
@@ -73,19 +74,21 @@ if (location.href.indexOf("/players/") != -1){
 			totSkill[1] = totSkill[1]*1 + skills[i]*1;
 		}
 		totSkill[0] = totSkill[0] / 200; 
+
 		if(positionIndex == 13){
-		totSkill[1] = totSkill[1]*100/220;
+			totSkill[1] = totSkill[1]*100/220;
 		} else {
-		totSkill[1] = totSkill[1]*100/280;
+			totSkill[1] = totSkill[1]*100/280;
 		}
+
 		totSkill[1] = (totSkill[0] - totSkill[1]);
 		totSkill[0] = Math.round(totSkill[0]*1000)/1000;
 		totSkill[1] = Math.round(totSkill[1]*1000)/1000;	
 		
 		//set pieces
 		totSkill[2][0] = ((skills[8]*2+skills[13]*2)+skills[9]*1)/5;// corner
-		totSkill[2][1] = ((skills[12]*2+skills[13]*2)+skills[9]*1)/5;// Frk
-		totSkill[2][2] = ((skills[11]*2+skills[13]*2)+skills[9]*1)/5;// pen
+		totSkill[2][1] = ((skills[12]*2+skills[13]*2)+skills[9]*1)/5;// Free kick
+		totSkill[2][2] = ((skills[11]*2+skills[13]*2)+skills[9]*1)/5;// penalty
 		
 		if(positionIndex != 13){ // is not goalkeeper is defender
 			totSkill[3][0] =(skills[3]*3+skills[6]*3+skills[5]*3+skills[4]*2+skills[2]*2+skills[1]*1)/280;
@@ -100,7 +103,7 @@ if (location.href.indexOf("/players/") != -1){
 			totSkill[3][3] = Math.round(totSkill[3][3]*100);
 			totSkill[3][4] = Math.round(totSkill[3][4]*100);
 			
-			//calculo de potencial para estilo de jogo
+			// calculate the potential game style
 			
 			totSkill[4][0] = (skills[2]*3+skills[1]*3+skills[7]*3+skills[6]*2+skills[5]*2)/260;
 			totSkill[4][1] = (skills[2]*3+skills[9]*3+skills[8]*3+skills[5]*2+skills[1]*2+skills[0]*1)/280;
@@ -118,16 +121,16 @@ if (location.href.indexOf("/players/") != -1){
 			finCab = Math.round((skills[10]*3+skills[0]*2+skills[6]*2+skills[5]*1+skills[2]*1)/180*100);
 			finRem = Math.round((skills[13]*3+skills[9]*3+skills[11]*2+skills[6]*1+skills[1]*1)/200*100);
 		
-			//calculo de potencial para estilo de finalização
+			// calculate the potential attacking style
 			
-			if(positionIndex  == 12){//F
+			if(positionIndex == 12){//F
 			totSkill[4][0] = totSkill[4][0]*0.2 + ((finReg*3+finCab*2+finRem*1)/6)*0.8;
 			totSkill[4][1] = totSkill[4][1]*0.2 + ((finReg*2+finCab*3+finRem*1)/6)*0.8;
 			totSkill[4][2] = totSkill[4][2]*0.2 + ((finReg*3+finCab*1+finRem*2)/6)*0.8;
 			totSkill[4][3] = totSkill[4][3]*0.2 + ((finReg*1+finCab*4+finRem*1)/6)*0.8;
 			totSkill[4][4] = totSkill[4][4]*0.2 + ((finReg*4+finCab*1+finRem*1)/6)*0.8; 
 			}
-			if(positionIndex >= 9 && positionIndex <= 11){//MO
+			if(positionIndex >= 9 && positionIndex <= 11){//OM
 			totSkill[4][0] = totSkill[4][0]*0.45 + ((finReg*3+finCab*2+finRem*1)/6)*0.55;
 			totSkill[4][1] = totSkill[4][1]*0.45 + ((finReg*2+finCab*3+finRem*1)/6)*0.55;
 			totSkill[4][2] = totSkill[4][2]*0.45 + ((finReg*3+finCab*1+finRem*2)/6)*0.55;
@@ -154,7 +157,7 @@ if (location.href.indexOf("/players/") != -1){
 			totSkill[4][2] = totSkill[4][2]*0.90 + ((finReg*3+finCab*1+finRem*2)/6)*0.10;
 			totSkill[4][3] = totSkill[4][3]*0.90 + ((finReg*1+finCab*4+finRem*1)/6)*0.10;
 			totSkill[4][4] = totSkill[4][4]*0.90 + ((finReg*4+finCab*1+finRem*1)/6)*0.10; 			
-			//para diferentes posições diferentes pesos
+			// different weights to different positions
 			}
 			
 			totSkill[4][0] = Math.round(totSkill[4][0]);
@@ -215,46 +218,46 @@ if (location.href.indexOf("/players/") != -1){
 	};
 
 	function computeSK(skills){
-	var SKs = [0, 0];
-	var Gok = [0, 0];
-	var setP = [0, 0, 0];
-	var defending = [0, 0, 0, 0, 0];
-	var gameplayStyle = [0, 0, 0, 0, 0];
-	var pos = 0;
-	var positionCell = document.getElementsByClassName("favposition long")[0].childNodes;
-	var positionArray = [];
-	if (positionCell.length == 1){
-			positionArray[0] = positionCell[0].textContent;
-	} else if (positionCell.length == 2){
-			positionArray[0] = positionCell[0].textContent + positionCell[1].textContent;
-	} else if (positionCell[1].className == "split"){
-			positionArray[0] = positionCell[0].textContent + positionCell[3].textContent;
-			positionArray[1] = positionCell[2].textContent + positionCell[3].textContent;
-	} else if (positionCell[3].className == "f"){
-			positionArray[0] = positionCell[0].textContent + positionCell[1].textContent;
-			positionArray[1] = positionCell[3].textContent;
-	} else {
-			positionArray[0] = positionCell[0].textContent + positionCell[1].textContent;
-			positionArray[1] = positionCell[0].textContent + positionCell[3].textContent;
-	}
-	for (var i = 0; i < positionArray.length; i++){
-			var positionIndex = document.findPositionIndex(positionArray[i]);
-			var skillAntes=0;
-			if (positionIndex > -1) {
-				var resultado = document.calculateSkill(positionIndex, skills);
-				SKs[i] = resultado[0];
-				Gok[i] = resultado[1];
-				setP = resultado[2];
-				defending = resultado[3];
-				if(resultado[0]>=skillAntes){
-					gameplayStyle = resultado[4];
-				} 
-				Pos = positionIndex;
-				skillAntes = resultado[0];
-			}
-	}
-	
-	return [SKs,Gok,Pos,setP,defending,gameplayStyle];
+		var SKs = [0, 0];
+		var Gok = [0, 0];
+		var setP = [0, 0, 0];
+		var defending = [0, 0, 0, 0, 0];
+		var gameplayStyle = [0, 0, 0, 0, 0];
+		var Pos = 0;
+		var positionCell = document.getElementsByClassName("favposition long")[0].childNodes;
+		var positionArray = [];
+		if (positionCell.length == 1){
+				positionArray[0] = positionCell[0].textContent;
+		} else if (positionCell.length == 2){
+				positionArray[0] = positionCell[0].textContent + positionCell[1].textContent;
+		} else if (positionCell[1].className == "split"){
+				positionArray[0] = positionCell[0].textContent + positionCell[3].textContent;
+				positionArray[1] = positionCell[2].textContent + positionCell[3].textContent;
+		} else if (positionCell[3].className == "f"){
+				positionArray[0] = positionCell[0].textContent + positionCell[1].textContent;
+				positionArray[1] = positionCell[3].textContent;
+		} else {
+				positionArray[0] = positionCell[0].textContent + positionCell[1].textContent;
+				positionArray[1] = positionCell[0].textContent + positionCell[3].textContent;
+		}
+		for (var i = 0; i < positionArray.length; i++){
+				var positionIndex = document.findPositionIndex(positionArray[i]);
+				var skillAntes=0;
+				if (positionIndex > -1) {
+					var resultado = document.calculateSkill(positionIndex, skills);
+					SKs[i] = resultado[0];
+					Gok[i] = resultado[1];
+					setP = resultado[2];
+					defending = resultado[3];
+					if(resultado[0]>=skillAntes){
+						gameplayStyle = resultado[4];
+					} 
+					Pos = positionIndex;
+					skillAntes = resultado[0];
+				}
+		}
+		
+		return [SKs,Gok,Pos,setP,defending,gameplayStyle];
 	}
 	
 	document.createTR = function(table, SKarray) {
@@ -303,7 +306,7 @@ if (location.href.indexOf("/players/") != -1){
 		table.appendChild(tr);
 	};
 	
-document.createTRSetPieces = function(table, nome, SK) {
+	document.createTRSetPieces = function(table, nome, SK) {
 		var tr = document.createElement("tr");
 		var th = document.createElement("th");
 		th.innerHTML = "";
@@ -351,24 +354,18 @@ document.createTRSetPieces = function(table, nome, SK) {
 		var gameplayStyle = computeSK(skillArray)[5];
 		document.createTR(playerTable, SKs);
 		document.createTRGok(playerTable, Goks);
+
 		if(computeSK(skillArray)[2] != 13){
-		
-		document.createTRtwoSides(playerTable, "Def. Direct", defending[0]+"%", "Play Direct", gameplayStyle[0]+"%");
-		document.createTRtwoSides(playerTable, "Def. Wings", defending[1]+"%", "Play Wings", gameplayStyle[1]+"%");
-		document.createTRtwoSides(playerTable, "Def. Shorpassing", defending[2]+"%", "Play Shorpassing", gameplayStyle[2]+"%");
-		document.createTRtwoSides(playerTable, "Def. Long Balls", defending[3]+"%", "Play Long Balls", gameplayStyle[3]+"%");
-		document.createTRtwoSides(playerTable, "Def. Through Balls", defending[4]+"%", "Play Through Balls", gameplayStyle[4]+"%");
+			document.createTRtwoSides(playerTable, "Def. Direct", defending[0]+"%", "Play Direct", gameplayStyle[0]+"%");
+			document.createTRtwoSides(playerTable, "Def. Wings", defending[1]+"%", "Play Wings", gameplayStyle[1]+"%");
+			document.createTRtwoSides(playerTable, "Def. Shorpassing", defending[2]+"%", "Play Shorpassing", gameplayStyle[2]+"%");
+			document.createTRtwoSides(playerTable, "Def. Long Balls", defending[3]+"%", "Play Long Balls", gameplayStyle[3]+"%");
+			document.createTRtwoSides(playerTable, "Def. Through Balls", defending[4]+"%", "Play Through Balls", gameplayStyle[4]+"%");
 
-		
-			
-		document.createTRSetPieces(playerTable, "Corner Kicks 0-20", setPieces[0]);
-		document.createTRSetPieces(playerTable, "Free Kicks 0-20", setPieces[1]);
-		document.createTRSetPieces(playerTable, "Penalty Kicks 0-20", setPieces[2]); 
-
-
+			document.createTRSetPieces(playerTable, "Corner Kicks 0-20", setPieces[0]);
+			document.createTRSetPieces(playerTable, "Free Kicks 0-20", setPieces[1]);
+			document.createTRSetPieces(playerTable, "Penalty Kicks 0-20", setPieces[2]); 
 		}
 		
 	})();
-	
-
 }
